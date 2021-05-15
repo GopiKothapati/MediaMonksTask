c534d433515f003431f2bc700f54e1964a32e1d0
GopiKothapati <gopi11bf1a0453@gmail.com>
2021-05-15 05:47:20 +0530

    Renamed AlbumlListViewController to AlbumListViewController

--- MediaMonksTask/Album List/AlbumListViewController.swift
+++ MediaMonksTask/Album List/AlbumListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AlbumlListViewController: UIViewController {
+class AlbumListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
