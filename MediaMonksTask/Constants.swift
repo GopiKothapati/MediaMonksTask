442b55184abeb2bd155f2a9a21ad17b0976d9bca
GopiKothapati <gopi11bf1a0453@gmail.com>
2021-05-15 06:31:24 +0530

    Added Constants File

---
+++ MediaMonksTask/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MediaMonksTask
+//
+//  Created by 1634391 on 15/05/21.
+//
+
+import Foundation
+
+enum ReuseID: String {
+    case albumsCell = "AlbumCell"
+    case photosListCell = "PhotosListCell"
+}
