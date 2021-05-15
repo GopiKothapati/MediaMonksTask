25f54061f7f639ca65260f5002ec71b10abed41d
GopiKothapati <gopi11bf1a0453@gmail.com>
2021-05-15 06:31:04 +0530

    Added ViewControllers for AlbumsList, Photos List and Photo Details in Storyboard

---
+++ MediaMonksTask/Album List/AlbumListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumListTableViewCell.swift
+//  MediaMonksTask
+//
+//  Created by 1634391 on 15/05/21.
+//
+
+import UIKit
+
+class AlbumListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MediaMonksTask/Photos List/PhotosListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosListCollectionViewCell.swift
+//  MediaMonksTask
+//
+//  Created by 1634391 on 15/05/21.
+//
+
+import UIKit
+
+class PhotosListCollectionViewCell: UICollectionViewCell {
+    
+}
